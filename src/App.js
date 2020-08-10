c06b6dbcbb3637d4ff222b854f1df647a42dc1b0
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-10 20:24:36 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   };
   render() {
     return (
-      <Router>
+      <Router basename='https://ralucabaksai.github.io/portofolio'>
         <div className='App'>
           <NavBar
             drawerClickHandler={this.drawerTaggleClickHandler}
