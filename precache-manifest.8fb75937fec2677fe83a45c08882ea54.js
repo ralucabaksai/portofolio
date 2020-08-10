52b4f5eeb1926f57e2c5716ae0e9c1c72e77d0da
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-10 16:37:09 +0300

    Updates

---
+++ precache-manifest.8fb75937fec2677fe83a45c08882ea54.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d48bf78cb9b158f96c43d9f8a8e47a",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "6d4448aae876bb06c8b6",
+    "url": "/portofolio/static/css/main.48b074f6.chunk.css"
+  },
+  {
+    "revision": "a20c1b5a9644abb4420f",
+    "url": "/portofolio/static/js/2.d7e8d4ec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portofolio/static/js/2.d7e8d4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4448aae876bb06c8b6",
+    "url": "/portofolio/static/js/main.4f3f83e4.chunk.js"
+  },
+  {
+    "revision": "f49303006df0205af3da",
+    "url": "/portofolio/static/js/runtime-main.74c6be3c.js"
+  },
+  {
+    "revision": "8f7b79f67debbfc00a723f30f90acbb7",
+    "url": "/portofolio/static/media/project-blog1.8f7b79f6.jpg"
+  },
+  {
+    "revision": "c3861b03d4946fc5ec98cc620516eb89",
+    "url": "/portofolio/static/media/project-canvas1.c3861b03.jpg"
+  },
+  {
+    "revision": "5e1734aa9f988271c72c6565ca499d92",
+    "url": "/portofolio/static/media/project-game1.5e1734aa.jpg"
+  },
+  {
+    "revision": "e81dc357417fa7d71ead6745de0a234b",
+    "url": "/portofolio/static/media/project-infinite-scroll1.e81dc357.jpg"
+  },
+  {
+    "revision": "1547c113623c9154bc7e4f91d5127533",
+    "url": "/portofolio/static/media/project-tic-tac-toe1.1547c113.jpg"
+  },
+  {
+    "revision": "247f3e2e506c2d2aa14def8293405944",
+    "url": "/portofolio/static/media/project-weather-app1.247f3e2e.jpg"
+  }
+]);
