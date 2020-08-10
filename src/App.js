6c0f486ee82967cc52ff6d046bfc387ee7d6016e
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-10 20:59:10 +0300

    v6

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   };
   render() {
     return (
-      <Router basename='/portofolio'>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className='App'>
           <NavBar
             drawerClickHandler={this.drawerTaggleClickHandler}
